72f9c049172ec1b0580813dcd5842150c3e05fe8
Prateek Ranjan <prateekranjan766@gmail.com>
2021-01-08 12:23:29 +0530

    Myblogs bug fixed

--- client/src/components/blogs/my-blogs/Blogs.jsx
+++ client/src/components/blogs/my-blogs/Blogs.jsx
@@ -31,7 +31,26 @@ const Blogs = (props) => {
   };
 
   if (personalBlogs !== null && personalBlogs.length === 0 && !loading) {
-    return <h4>Please add a BLOG!</h4>;
+    return (
+      <div className="link__div">
+        <Link className="link" to="/create-new">
+          <i className="fas fa-plus"></i> Create Blog
+        </Link>
+        <form className="search__form">
+          <label className="search__form__label">
+            <i className="fas fa-search" />
+            <input
+              className="search__bar"
+              type="text"
+              ref={text}
+              name={text}
+              placeholder="Search for Blog title"
+              onChange={onChange}
+            />
+          </label>
+        </form>
+      </div>
+    );
   }
 
   return (
